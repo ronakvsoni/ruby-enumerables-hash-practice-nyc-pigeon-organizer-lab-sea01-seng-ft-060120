223a64cb8fd0f51e68587ad68aec495d46d6fac2
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-28 17:26:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,16 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  binding.pry
   # write your code here!
   final_output = data.each_object({}) do |(key,value), final_array|
+    value.each do |inner_key, name|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+end
+if !final_array[name][key]
+  !final_array[name][key] = []
+end
+
+
 binding.pry
 end
